dd00d9a6c8096e45e33af8907e5b2b492ca97db9
Mike Splain <msplain@paypal.com>
2015-01-30 21:13:36 -0500

    Kill process before exiting

--- openvas_base/bin/setup.sh
+++ openvas_base/bin/setup.sh
@@ -9,8 +9,11 @@ openvas-nvt-sync
 openvas-scapdata-sync
 openvas-certdata-sync
 openvas-mkcert-client -n -i
-openvasmd --rebuild --progress
+openvasmd --rebuild
 openvasmd --create-user=admin --role=Admin
 openvasmd --user=admin --new-password=openvas
+
+# At this point, usually openvassd locks up so lets kill it
+ps aux | grep openvassd| awk '{print $2}' |xargs kill -9
 
 echo "Finished setup..."

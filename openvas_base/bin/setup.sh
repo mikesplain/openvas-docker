5be04c2fea0dbe43934e37622870e8ce7c75eb35
Mike Splain <msplain@paypal.com>
2015-03-23 16:02:24 -0400

    remove rebuilds

--- openvas_base/bin/setup.sh
+++ openvas_base/bin/setup.sh
@@ -9,13 +9,10 @@ openvas-nvt-sync
 openvas-scapdata-sync
 openvas-certdata-sync
 openvas-mkcert-client -n -i
-openvasmd --rebuild
 openvasmd --create-user=admin --role=Admin
 openvasmd --user=admin --new-password=openvas
 
 # At this point, usually openvassd locks up so lets kill it
 ps aux | grep openvassd| awk '{print $2}' |xargs kill -9
-
-openvasmd --rebuild
 
 echo "Finished setup..."

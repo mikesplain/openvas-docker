5f456612baccdc689c6cf1918fce63d36f3e1cdf
Mike Splain <mike.splain@cylentsystems.com>
2015-07-11 10:58:03 -0400

    Fix

--- test.sh
+++ test.sh
@@ -10,4 +10,5 @@ until docker logs openvas | grep -E 'It seems like your OpenVAS-7 installation i
   echo "=========================================================================";
   sleep 2 ;
 done
+
 docker logs openvas | grep -E 'It seems like your OpenVAS-7 installation is OK'

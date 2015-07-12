4a9f129c65a2d347be1945b8f661781eaa54a5de
Mike Splain <mike.splain@cylentsystems.com>
2015-07-12 14:42:03 -0400

    Update

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 docker run -d -p 443:443 -p 9390:9390 -p 9391:9391 --name openvas mikesplain/openvas
 
 echo "Waiting for startup to complete..."
-until docker top openvas | grep gsad; do \
+until docker logs openvas | grep -E 'It seems like your OpenVAS-7 installation is' do \
   echo "." ; \
   sleep 2 ; \
 done
 
-/openvas/openvas-check-setup
+docker logs openvas | grep -E 'It seems like your OpenVAS-7 installation is OK'

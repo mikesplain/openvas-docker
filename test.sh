863dea7e227071e9dbfbf509b2440b8cc5676f0b
Mike Splain <mike.splain@gmail.com>
2015-08-22 16:53:30 -0400

    Rename container so it doesn't pull from registry.

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 443:443 -p 9390:9390 -p 9391:9391 --name openvas mikesplain/openvas
+docker run -d -p 443:443 -p 9390:9390 -p 9391:9391 --name openvas openvas
 
 echo "Waiting for startup to complete..."
 until docker logs openvas | grep -E 'It seems like your OpenVAS-7 installation is'; do

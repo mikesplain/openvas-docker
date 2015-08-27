14a516456399f17dbf2a2e85b9392c83671a7340
Mike Splain <mike.splain@cylentsystems.com>
2015-08-27 17:06:33 -0400

    A little more logging

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,10 +9,13 @@ openvas-nvt-sync
 openvas-scapdata-sync
 openvas-certdata-sync
 openvas-mkcert-client -n -i
-openvasmd --rebuild --progress -v
+echo "Rebuilding Openvasmd..."
+openvasmd --rebuild -v
+echo "Creating Admin user..."
 openvasmd --create-user=admin --role=Admin
+echo "Setting Admin user password..."
 openvasmd --user=admin --new-password=openvas
-
+echo "Killing some locked up openvassd's"
 # At this point, usually openvassd locks up so lets kill it
 ps aux | grep openvassd| awk '{print $2}' |xargs kill -9
 

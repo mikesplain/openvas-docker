b556288f97507227faa20c7978de96033a8b5473
Mike Splain <mike.splain@cylentsystems.com>
2015-08-28 16:26:15 -0400

    Add timeout to startup

--- bin/start.sh
+++ bin/start.sh
@@ -10,7 +10,17 @@ echo "Starting gsad"
 echo "Starting Openvassd"
 ./openvassd
 echo "Rebuilding openvasmd"
-openvasmd --rebuild -v
+n=1
+until [ $n -eq 4 ]
+do
+         timeout 5m openvasmd --rebuild -v;
+        if [ $? -eq 0 ]; then
+                 break;
+         fi
+         echo "Rebuild failed, attempt: $n"
+         n=$[$n+1]
+done
+
 echo "Checking setup"
 /openvas/openvas-check-setup --v7
 

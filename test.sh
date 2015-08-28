236a095173bc32285d08a387baf8e5a8e21e235f
Mike Splain <mike.splain@cylentsystems.com>
2015-08-27 21:46:58 -0400

    Add debugging

--- test.sh
+++ test.sh
@@ -5,6 +5,9 @@ docker run -d -p 443:443 -p 9390:9390 -p 9391:9391 --name openvas openvas
 echo "Waiting for startup to complete..."
 until docker logs openvas | grep -E 'It seems like your OpenVAS-7 installation is'; do
   echo "." ;
+  echo "=========================================================================";
+  docker logs openvas
+  echo "=========================================================================";
   sleep 2 ;
 done
 

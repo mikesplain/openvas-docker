adafd4d9e03ba9ce15f2d566a4981d6e1d29c8ac
Mike Splain <mike.splain@gmail.com>
2017-01-22 11:11:22 -0500

    Cleanup

--- 9/test.sh
+++ 9/test.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-docker run -d -p 4000:4000 -p 9390:9390 -p 9391:9391 --name openvas9 openvas9
+docker run -d -p 4000:4000 --name openvas9 openvas9
 
 echo "Waiting for startup to complete..."
-until docker logs --tail=15 openvas9 | grep -E 'It seems like your OpenVAS-9 installation is'; do
-  echo "." ;
-  echo "=========================================================================";
-  docker logs --tail=15 openvas9
-  echo "=========================================================================";
-  sleep 5 ;
+until docker logs openvas9 | grep -E 'It seems like your OpenVAS-9 installation is'; do
+  echo .
+  sleep 5
 done
 
-docker logs --tail=15 openvas9 | grep -E 'It seems like your OpenVAS-9 installation is OK'
+docker logs openvas9 | grep -E 'It seems like your OpenVAS-9 installation is OK'

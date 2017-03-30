0f1788a62f70e9f8eae04ae592bbbb08b7f5fb68
Jay Donigian <jay.donigian@ubnt.com>
2017-03-30 09:34:04 -0600

    cleaning up the Dockerfile

--- 9/run.sh
+++ 9/run.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
-#echo "Starting redis and openvas"
-#/usr/sbin/service redis-server start
-#/usr/sbin/service openvas-gsa start
-#/usr/sbin/service openvas-manager start
-#/usr/sbin/service openvas-scanner start
-/usr/sbin/service postfix restart
-#/usr/sbin/service mailutils start
-
 set -o nounset
 set -o errexit
 set -o pipefail
@@ -18,5 +10,3 @@ sed -i "s/MANDRILL_KEY/${MANDRILL_KEY}/g" /etc/postfix/sasl_passwd
 /usr/sbin/postmap /etc/postfix/sasl_passwd
 
 /usr/bin/supervisord
-
-

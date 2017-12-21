0ff1a46bf2b119e58f6bbf640b280771344ab268
Mike Splain <mike.splain@gmail.com>
2017-12-21 15:05:25 -0500

    Remove redirection

--- 9/ci.sh
+++ 9/ci.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 mkdir -p logs images
 
-docker build -t openvas9 . 2>&1 | tee logs/build.log
+docker build -t openvas9 . 
 docker save openvas9 | gzip -c  > images/openvas9.tar.gz
-./test.sh 2>&1 | tee logs/test.log
-docker logs openvas9 2>&1 | tee logs/container.log
+./test.sh 
+docker logs openvas9

ae431b0666418bee229699b748cfb15ce87aeafb
Mike Splain <mike.splain@gmail.com>
2017-12-28 11:21:47 -0500

    Fix exit code

--- 9/ci.sh
+++ 9/ci.sh
@@ -8,4 +8,9 @@ docker tag openvas9 quay.io/mikesplain/openvas:travis-${TRAVIS_BUILD_ID}
 docker push quay.io/mikesplain/openvas:travis-${TRAVIS_BUILD_ID}
 ./test.sh 
 
+if [ $? -eq 1]; then
+    echo "Test failure. Look in log to debug."
+    exit 1
+fi
+
 echo "Test Complete!"

c569fdffbe5a3765c4e1fb77bf88f49ab258677f
Mike Splain <mike.splain@gmail.com>
2017-12-28 11:17:43 -0500

    Fix port issues

--- 9/test.sh
+++ 9/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d -p 4000:4000 --name openvas9 openvas9
+docker run -d -p 8443:443 --name openvas9 openvas9
 
 echo "Waiting for startup to complete..."
 until docker logs openvas9 | grep -E 'It seems like your OpenVAS-9 installation is'; do
@@ -8,7 +8,7 @@ until docker logs openvas9 | grep -E 'It seems like your OpenVAS-9 installation 
   sleep 5
 done
 
-if $(curl -k https://localhost:4000/login/login.html | grep -q "Greenbone Security Assistant"); then
+if $(curl -k https://localhost:8443/login/login.html | grep -q "Greenbone Security Assistant"); then
   echo "Greenbone started successfully!"
 else
   echo "Greenbone couldn't be found. There's probably something wrong"

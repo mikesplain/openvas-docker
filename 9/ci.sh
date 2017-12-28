a21959310d940f5bbe663e37bd625cdaea09fc26
Mike Splain <mike.splain@gmail.com>
2017-12-28 13:03:54 -0500

    Fix typo

--- 9/ci.sh
+++ 9/ci.sh
@@ -8,7 +8,7 @@ docker tag openvas9 quay.io/mikesplain/openvas:travis-${TRAVIS_BUILD_ID}
 docker push quay.io/mikesplain/openvas:travis-${TRAVIS_BUILD_ID}
 ./test.sh 
 
-if [ $? -eq 1]; then
+if [ $? -eq 1 ]; then
     echo "Test failure. Look in log to debug."
     exit 1
 fi

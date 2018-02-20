1b3fd9829a9587b35e9a6b873a4032ed0faec689
Mike Splain <mike.splain@gmail.com>
2018-02-20 10:30:19 -0500

    Fix ci

--- 9/ci.sh
+++ 9/ci.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+echo "Setup"
+
+if [ -n "$QUAY_PASSWORD" ]; then
+    docker login -u="${QUAY_USER}" -p="${QUAY_PASSWORD}" quay.io
+fi
+
 cd $(dirname $0)
 mkdir -p logs images
 
-docker build -t openvas9 . 
+docker build -t openvas9 .
 docker tag openvas9 quay.io/mikesplain/openvas:travis-${TRAVIS_BUILD_ID}
-docker push quay.io/mikesplain/openvas:travis-${TRAVIS_BUILD_ID}
-./test.sh 
+
+if [ -n "$QUAY_PASSWORD" ]; then
+    docker push quay.io/mikesplain/openvas:travis-${TRAVIS_BUILD_ID}
+fi
+
+./test.sh
 
 if [ $? -eq 1 ]; then
     echo "Test failure. Look in log to debug."

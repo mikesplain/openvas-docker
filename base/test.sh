409dea18bd28320e2ea3a2c3d48a12b8bcec4365
Mike Splain <mike.splain@gmail.com>
2018-08-22 13:46:57 -0400

    Fix

--- base/test.sh
+++ base/test.sh
@@ -3,7 +3,7 @@
 docker-compose -f ../docker-compose.yml up -d
 
 echo "Waiting for startup to complete..."
-until docker logs openvas-docker_openvas_1 | grep -E 'It seems like your OpenVAS-9 installation is'; do
+until docker logs openvasdocker_openvas_1 | grep -E 'It seems like your OpenVAS-9 installation is'; do
   echo .
   sleep 5
 done

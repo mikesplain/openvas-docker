1bd28c4a4c11a87ea1d6034d96046333df972ad5
Mike Splain <mike.splain@gmail.com>
2021-04-16 11:54:55 -0400

    Get python version

--- gvm20/install-gvm5.sh
+++ gvm20/install-gvm5.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+PY3VER=`python3 --version | grep -o [0-9]\.[0-9]`
+mkdir -p /opt/gvm/lib/python$PY3VER/site-packages/
+export PYTHONPATH=/opt/gvm/lib/python$PY3VER/site-packages
+
 cd /tmp/gvm-source/ospd
 python3 setup.py install --prefix=/opt/gvm
 cd ../ospd-openvas

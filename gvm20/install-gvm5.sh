3d0077edc4eeebb14843e4e0c73894d02829e264
Mike Splain <mike.splain@gmail.com>
2021-04-25 11:43:32 -0400

    Update

--- gvm20/install-gvm5.sh
+++ gvm20/install-gvm5.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PKG_CONFIG_PATH=/opt/gvm/lib/pkgconfig:$PKG_CONFIG_PATH
+
 PY3VER=`python3 --version | grep -o [0-9]\.[0-9]`
 mkdir -p /opt/gvm/lib/python$PY3VER/site-packages/
 export PYTHONPATH=/opt/gvm/lib/python$PY3VER/site-packages
@@ -8,3 +10,7 @@ cd /tmp/gvm-source/ospd
 python3 setup.py install --prefix=/opt/gvm
 cd ../ospd-openvas
 python3 setup.py install --prefix=/opt/gvm
+
+# Move earlier in installers
+
+mkdir -p /opt/gvm/var/run/
